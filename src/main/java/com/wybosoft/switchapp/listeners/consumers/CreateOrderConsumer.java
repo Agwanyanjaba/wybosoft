91c0ecd5fa308b1aafccc7a6c71e45a13b3bee8d
Agwanyanjaba <bonkewycliffe@yahoo.com>
2024-03-06 01:05:39 +0300

    feat: add CreateOrderConsumer -update: refactor files names

--- src/main/java/com/wybosoft/switchapp/listeners/consumers/CreateOrderConsumer.java
+++ src/main/java/com/wybosoft/switchapp/listeners/consumers/CreateOrderConsumer.java
@@ -1,2 +1,22 @@
-package com.wybosoft.switchapp.listeners.consumers;public class CreateOrderConsumer {
+package com.wybosoft.switchapp.listeners.consumers;
+
+
+import com.wybosoft.switchapp.models.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service("NotificationService")
+public class CreateOrderConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(CreateOrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.order.topic.create-order}", containerFactory="NotificationContainerFactory")
+    public void createOrderListener(@Payload Order order, Acknowledgment ack) {
+        log.info("Notification service received order {} ", order);
+        ack.acknowledge();
+    }
 }
--- src/main/java/com/wybosoft/switchapp/models/Order.java
+++ src/main/java/com/wybosoft/switchapp/models/Order.java
@@ -1,2 +1,20 @@
-package com.wybosoft.switchapp.models;public class Order {
+package com.wybosoft.switchapp.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Order {
+
+    @Getter
+    @Setter
+    private String orderID;
+
+    @Getter
+    @Setter
+    private Date dateOfCreation;
+
+    @Getter
+    @Setter
+    private String content;
+
 }
